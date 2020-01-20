e0481d7db2f2cfc4342d9aefbae06966eeaad05e
Relsell <relsellglobal@gmail.com>
2020-01-20 07:25:00 +0530

    Initial Commit

---
+++ LoginScreenDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginScreenDemo
+//
+//  Created by ANIL KUKRETI on 20/01/20.
+//  Copyright © 2020 Relsell Global. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
